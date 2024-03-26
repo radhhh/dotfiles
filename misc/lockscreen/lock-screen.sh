9c81c47c0024c6614755f1c59b58f88904f65e63
radhhh <123277320+radhhh@users.noreply.github.com>
2024-03-27 00:55:58 +0700

    Routine Update

--- misc/lockscreen/lock-screen.sh
+++ misc/lockscreen/lock-screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u radhya WAYLAND_DISPLAY=wayland-1 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/swaylock --screenshots & disown && /usr/bin/sleep 2
+sudo -u radhya WAYLAND_DISPLAY=wayland-1 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/swaylock & disown && /usr/bin/sleep 2
